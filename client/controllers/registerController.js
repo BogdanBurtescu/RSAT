074be1c2b66a7b67d218e1b6c6b941c87a5b2ff8
BogdanBurtescu <bogdan.burtescu@jlg.ro>
2015-12-28 00:06:40 +0200

    Implemented form password validation

--- client/controllers/registerController.js
+++ client/controllers/registerController.js
@@ -1,4 +1,3 @@
-
 angular.module('myApp').controller('registerController',
     ['$scope', '$location', 'AuthService',
         function ($scope, $location, AuthService) {
@@ -13,28 +12,45 @@ angular.module('myApp').controller('registerController',
                 $scope.dateOfCreation = new Date(Date.UTC(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDay()));
 
                 // call register from service
-                AuthService.register($scope.registerForm.username,
-                                     $scope.registerForm.password,
-                    $scope.registerForm.firstName,
-                    $scope.registerForm.lastName,
-                    $scope.dateOfCreation
-                    )
-                    // handle success
-                    .then(function () {
-                        toastr.success("You have succesfully registered!");
-                        $location.path('/login');
-                        $scope.disabled = false;
-                        $scope.registerForm = {};
-                    })
-                    // handle error
-                    .catch(function () {
-                        toastr.success("Something went wrong!");
-                        $scope.error = true;
-                        $scope.errorMessage = "Something went wrong!";
-                        $scope.disabled = false;
-                        $scope.registerForm = {};
-                    });
+                if(checkPasswordMatch() === true){
+                    AuthService.register($scope.registerForm.username,
+                        $scope.registerForm.password,
+                        $scope.registerForm.firstName,
+                        $scope.registerForm.lastName,
+                        $scope.dateOfCreation
+                        )
+                        // handle success
+                        .then(function () {
+                            toastr.success("You have succesfully registered!");
+                            $location.path('/login');
+                            $scope.disabled = false;
+                            $scope.registerForm = {};
+                        })
+                        // handle error
+                        .catch(function () {
+                            toastr.error("Something went wrong!");
+                            $scope.error = true;
+                            $scope.errorMessage = "Something went wrong!";
+                            $scope.disabled = false;
+                        });
+                }else{
+                    toastr.error("Passwords must match!");
+                    $scope.disabled = false;
+
+                }
+
 
             };
+
+            function checkPasswordMatch(){
+                var result;
+                if($scope.registerForm.password === $scope.registerForm.passwordConfirmation)
+                {
+                    result = true;
+                }else{
+                    result = false;
+                }
+                return result;
+            }
 
         }]);
