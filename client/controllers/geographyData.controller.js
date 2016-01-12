f7380618b819c646d98261e0a16ae80052707240
BogdanBurtescu <bogdan.burtescu@jlg.ro>
2016-01-12 15:49:54 +0200

    Added sample Angular table to the geographyData.view

--- client/controllers/geographyData.controller.js
+++ client/controllers/geographyData.controller.js
@@ -1,8 +1,41 @@
 angular.module('myApp').controller('geographyDataController',
     ['$scope', '$location',
-        function ($scope, $location, NgTableParams) {
+        function ($scope, $location) {
 
-            $scope.cacat = "blablabla";
+            $scope.sortType     = 'name'; // set the default sort type
+            $scope.sortReverse  = false;  // set the default sort order
+            $scope.searchGeographicalEntity   = '';     // set the default search/filter term
+
+            // create the list of sushi rolls
+            $scope.geographicalEntities = [
+                { item1: 'Cali Roll', item2: 'Crgrab', item3: 2, item4: 2 },
+                { item1: 'adasdadf', item2: 'gfdg', item3: 5, item4: 2  },
+                { item1: 'Cali dfsfgs', item2: 't45y4', item3: 99, item4: 2  },
+                { item1: 'Cafgdgl', item2: 'fgdg4', item3: 45, item4: 2  },
+                { item1: 'Catyjhl', item2: 'rg34g', item3: 546 , item4: 2 },
+                { item1: 'Cali Roll', item2: 'Crgrab', item3: 2, item4: 2 },
+                { item1: 'adasdadf', item2: 'gfdg', item3: 5, item4: 2  },
+                { item1: 'Cali dfsfgs', item2: 't45y4', item3: 99, item4: 2  },
+                { item1: 'Cafgdgl', item2: 'fgdg4', item3: 45, item4: 2  },
+                { item1: 'Catyjhl', item2: 'rg34g', item3: 546 , item4: 2 },
+                { item1: 'Cali Roll', item2: 'Crgrab', item3: 2, item4: 2 },
+                { item1: 'adasdadf', item2: 'gfdg', item3: 5, item4: 2  },
+                { item1: 'Cali dfsfgs', item2: 't45y4', item3: 99, item4: 2  },
+                { item1: 'Cafgdgl', item2: 'fgdg4', item3: 45, item4: 2  },
+                { item1: 'Catyjhl', item2: 'rg34g', item3: 546 , item4: 2 },
+                { item1: 'Cali Roll', item2: 'Crgrab', item3: 2, item4: 2 },
+                { item1: 'adasdadf', item2: 'gfdg', item3: 5, item4: 2  },
+                { item1: 'Cali dfsfgs', item2: 't45y4', item3: 99, item4: 2  },
+                { item1: 'Cafgdgl', item2: 'fgdg4', item3: 45, item4: 2  },
+                { item1: 'Catyjhl', item2: 'rg34g', item3: 546 , item4: 2 },
+                { item1: 'Cali Roll', item2: 'Crgrab', item3: 2, item4: 2 },
+                { item1: 'adasdadf', item2: 'gfdg', item3: 5, item4: 2  },
+                { item1: 'Cali dfsfgs', item2: 't45y4', item3: 99, item4: 2  },
+                { item1: 'Cafgdgl', item2: 'fgdg4', item3: 45, item4: 2  },
+                { item1: 'Catyjhl', item2: 'rg34g', item3: 546 , item4: 2 }
+
+            ];
+
 
         }]);
 
